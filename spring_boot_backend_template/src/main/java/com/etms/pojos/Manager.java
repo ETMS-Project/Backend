cedba5840a28b529ea99b8539eb1a05bac869ce2
shantanu <shantanupatil958@gmail.com>
2025-02-13 22:02:11 +0530

    commited manager services, manager pojo and manager controller

--- spring_boot_backend_template/src/main/java/com/etms/pojos/Manager.java
+++ spring_boot_backend_template/src/main/java/com/etms/pojos/Manager.java
@@ -3,10 +3,11 @@ package com.etms.pojos;
 import java.time.LocalDate;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -21,33 +22,15 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString(callSuper = true,exclude = {"projects","employees"})
 public class Manager extends Person {
-	
-	@Column(name = "first_name", length = 20) // column name , varchar(20)
-	private String firstName;
-	
-	@Column(name = "last_name", length = 20) // column name , varchar(20)
-	private String lastName;
-	
-	
-	
-	
-	@JsonFormat(pattern = "yyyy-MM-dd")
-	private LocalDate dob;
-	
-	
-    @OneToMany(mappedBy = "manager") //don't add cascade as when manager is removed tranfer
-    private List<Project> projects;// them to other manager
+
+	 
 
     @Override
     public String getRole(){
         return "MANAGER";
     }
    
-    //helper method to add project
-    public void addProject(Project project) {
-    	this.projects.add(project);
-    	project.setManager(this);
-    }
+ 
     
     
 }
--- spring_boot_backend_template/src/main/java/com/etms/services/ManagerService.java
+++ spring_boot_backend_template/src/main/java/com/etms/services/ManagerService.java
@@ -2,15 +2,21 @@ package com.etms.services;
 
 import java.util.List;
 
-import com.etms.pojos.Employee;
-import com.etms.pojos.Manager;
+import com.etms.dtos.ApiResponse;
+import com.etms.dtos.PedingTaskResponseDto;
+import com.etms.dtos.TaskRequestDto;
+import com.etms.dtos.TaskResponseDto;
 
 public interface ManagerService {
 
-	Employee createEmployee(Employee employee);
+	List<TaskResponseDto> getAllTask(Long manId);
 
-	List<Employee> getAllEmployees();
+	TaskResponseDto createTask(TaskRequestDto taskdto);
 
-	
+	ApiResponse assignTask(Long taskId, Long empId);
+
+	TaskResponseDto updateTask(Long taskId, TaskRequestDto updatedTaskDto);
+
+	List<PedingTaskResponseDto> getAllPendingTask(Long manId);
 	
 }
